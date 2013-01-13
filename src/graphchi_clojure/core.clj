4c9567fa12c72dc45de974358cf2d1ec593201c4
David James Humphreys <davidjameshumphreys@gmail.com>
2013-01-13 15:07:59 +0000

    First attempt at translating PageRank.java
    It doesn't work yet.

--- src/graphchi_clojure/core.clj
+++ src/graphchi_clojure/core.clj
@@ -3,15 +3,42 @@
            [edu.cmu.graphchi.engine GraphChiEngine]
            [edu.cmu.graphchi.datablocks FloatConverter]))
 
-(defn create-program [update-fn]
+(def create-program
   (proxy
-      [GraphChiProgram]
-      []
-    (update [vertex context] (println "hai"))
+      [edu.cmu.graphchi.GraphChiProgram] []
+    
+    (update [vertex context]
+      (println vertex " " context)
+      (if (zero? (. context getIteration))
+        (. vertex setValue (float 1))
+        
+        (let [n (. vertex numInEdges)
+              sum (reduce +
+                          (doall
+                           (map
+                            #(. (. vertex inEdge %) getValue)
+                            (range 0 n))))]
+
+          (. vertex setValue (+ (* 0.85 sum) 0.15))))
+
+      (let [outValue (/ (. vertex getValue) (. vertex numOutEdges))
+            n (. vertex numOutEdges)
+            ]
+        (doall (map #(. (. vertex outEdge %) setValue outValue)))
+        )
+      )
     (beginInterval [context interval] (println "a") )
     (beginIteration [context])
     (endIteration [context])
     (endInterval [context interval])))
+
+
+#_(if
+            (or (zero? (mod (. vertex getId) 100000))
+                (#{8737 2914} (. vertex getId)))
+          
+          (println (. vertex getId) " => " (. vertex getValue))
+          )
 
 (defn run [file shards program iterations]
   (let [engine (new GraphChiEngine file shards)]
@@ -19,7 +46,10 @@
       (.setEdataConverter (new FloatConverter))
       (.setVertexDataConverter (new FloatConverter))
       (.setModifiesInedges false)
-      (.run program iterations)) ;; apparently an important optimisation
+      (.run program iterations)) ;; apparently an important
+    ;; optimisation
+
+    
     ))
 
 
@@ -27,4 +57,5 @@
   "I don't do a whole lot."
   [& args]
 
-  (println "Hello, World!"))
+  (run "edgesfoo/0.edges" 2 create-program 10)
+  (println "hai!"))
