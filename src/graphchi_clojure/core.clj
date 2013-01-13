b3916a0c31e73763f37161cc77a969c71207458a
Jennifer Smith <jennifer@home>
2013-01-13 20:11:17 +0000

    Made more functional

--- src/graphchi_clojure/core.clj
+++ src/graphchi_clojure/core.clj
@@ -8,39 +8,66 @@
 (set! *warn-on-reflection* true)
 
 (defn all-in-edges-vertices [vertex]
-  (vec
-   (map
-    #(. (. vertex inEdge %) getValue)
-    (range 0 (. vertex numInEdges))))
-  )
+  (map
+   #(. (. vertex inEdge %) getValue)
+   (range 0 (. vertex numInEdges))))
 
-(defn calc-new-vertex-value [vertex context]
-  (if (zero? (. context getIteration))
-    1.0
-    (let [sum (reduce + (all-in-edges-vertices vertex))]
-      (+ (* 0.85 sum) 0.15)
-      )
-    )
-  )
+(defn all-out-edges-vertices [vertex]
+  (map
+   #(. (. vertex outEdge %) getValue)
+   (range 0 (. vertex numOutEdges))))
 
-(def create-program
+
+
+(defn vertex-to-map [vertex]
+  {:value (.getValue vertex)
+   :in-edges (all-in-edges-vertices vertex)
+   :out-edges (all-out-edges-vertices vertex)})
+
+(defn update-in-edges [vertex in-edges]
+  (map
+   #(. (. vertex inEdge (int %1)) setValue %2)
+   (range)
+   in-edges))
+
+(defn update-out-edges [vertex out-edges]
+  (map
+   #(. (. vertex outEdge (int %1)) setValue %2)
+   (range)
+   out-edges))
+
+(defn update-vertex [vertex {:keys [value in-edges out-edges]}]
+  (doto vertex
+      (.setValue (float value))
+    (update-in-edges in-edges)
+    (update-out-edges out-edges)))
+
+
+(defn calc-new-vertex-value [{:keys [in-edges] :as vertex-map} iteration]
+  (if (zero? iteration)
+    (assoc vertex-map :value 1.0)
+    (let [sum (reduce + in-edges)]
+      (assoc vertex-map :value (+ (* 0.85 sum) 0.15)))))
+
+(defn calc-out-edges [{:keys [value out-edges] :as vertex-map}]
+  (let [n-out-edges (count out-edges)]
+    (if (zero? (count out-edges))
+      vertex-map
+      (let [
+            new-value (/ value (count out-edges))]
+        (assoc vertex-map :out-edges (repeat n-out-edges new-value))))))
+
+(defn page-rank [{:keys [in-edges] :as vertex-map} iteration]
+  (-> vertex-map
+      (calc-new-vertex-value iteration)
+      (calc-out-edges)))
+
+(defn create-program [update-fn]
   (proxy
       [edu.cmu.graphchi.GraphChiProgram] []
-    
-    (update [vertex context]      
-      (let [new-vertex-val (float (calc-new-vertex-value vertex context))
-            num-out-edges (int (. vertex numOutEdges))
-            outValue  (/  new-vertex-val
-                         num-out-edges)]
-        
-        (for [n (range 0 num-out-edges)]
-          (. (. vertex outEdge (int n)) setValue outValue)
-          )
-
-        (. vertex setValue  new-vertex-val)
-        )
-      )
-    
+    (update [vertex context]
+      (let [result (update-fn (vertex-to-map vertex) (.getIteration context))]
+        (update-vertex vertex result)))
     (beginInterval [context interval] )
     (beginIteration [context])
     (endIteration [context])
@@ -60,7 +87,6 @@
     (let [top20 (Toplist/topListFloat file 20)
           i (iterator-seq (. top20 iterator) )
           ]
-      
       (doall
        (map
         (fn [a b]
@@ -68,15 +94,8 @@
            :vertex (. b getVertexId)
            :value (. b getValue)}
           )
-        (range)
-        
-        i)
-       ))
-    ))
+        (range) i)))))
 
 (defn -main
-  "I don't do a whole lot."
   [& args]
-
-  (run "edgesfoo/0.edges" 2 create-program 10)
-  (println "hai!"))
+  (run "../facebook/0.edges" 2 (create-program page-rank) 30))
