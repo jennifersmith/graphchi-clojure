1c2e095a3adc3b25dfd6fc39269f75847bf49026
David James Humphreys <davidjameshumphreys@gmail.com>
2013-01-13 17:50:22 +0000

    Refactoring and cleaning the code.
    Made the example more functional.

--- src/graphchi_clojure/core.clj
+++ src/graphchi_clojure/core.clj
@@ -6,63 +6,45 @@
              IdFloat]))
 
 (set! *warn-on-reflection* true)
+
+(defn all-in-edges-vertices [vertex]
+  (vec
+   (map
+    #(. (. vertex inEdge %) getValue)
+    (range 0 (. vertex numInEdges))))
+  )
+
+(defn calc-new-vertex-value [vertex context]
+  (if (zero? (. context getIteration))
+    1.0
+    (let [sum (reduce + (all-in-edges-vertices vertex))]
+      (+ (* 0.85 sum) 0.15)
+      )
+    )
+  )
+
 (def create-program
   (proxy
       [edu.cmu.graphchi.GraphChiProgram] []
     
-    (update [vertex context]
-      (if (zero? (. context getIteration))
-        (. vertex setValue (float 1))
-
-        (let [n (. vertex numInEdges)
-              sum (reduce +
-                          (doall
-                           (map
-                            #(. (. vertex inEdge %) getValue)
-                            (range 0 n))))
-              ]
-          (. vertex setValue (float (+ (* 0.85 sum) 0.15)))
+    (update [vertex context]      
+      (let [new-vertex-val (float (calc-new-vertex-value vertex context))
+            num-out-edges (int (. vertex numOutEdges))
+            outValue  (/  new-vertex-val
+                         num-out-edges)]
+        
+        (for [n (range 0 num-out-edges)]
+          (. (. vertex outEdge (int n)) setValue outValue)
           )
-        )
 
-      
-      (let [outValue  (/ (float (. vertex getValue))  (int (. vertex numOutEdges)))
-            
-            ]
-        (for [n (range 0 (int (. vertex numOutEdges)))]
-          (. (. vertex outEdge (int n)) setValue (float 2))
-          )
+        (. vertex setValue  new-vertex-val)
         )
-      
       )
-    (beginInterval [context interval] (println "a") )
+    
+    (beginInterval [context interval] )
     (beginIteration [context])
     (endIteration [context])
     (endInterval [context interval])))
-
-        #_(let [n (. vertex numInEdges)
-              sum (reduce +
-                          (doall
-                           (map
-                            #(. (. vertex inEdge %) getValue)
-                            (range 0 n))))
-              ]
-          
-          (. vertex setValue (float (+ (* 0.85 sum) 0.15)))
-  
-          (let [outValue (/ (. vertex getValue) (. vertex numOutEdges))
-                n (. vertex numOutEdges)
-                ]
-            (doall (map #(. (. vertex outEdge %) setValue outValue)))
-            ))
-
-
-#_(if
-            (or (zero? (mod (. vertex getId) 100000))
-                (#{8737 2914} (. vertex getId)))
-          
-          (println (. vertex getId) " => " (. vertex getValue))
-          )
 
 (defn run [file shards program iterations]
   (let [engine (new GraphChiEngine file shards)]
@@ -78,16 +60,19 @@
     (let [top20 (Toplist/topListFloat file 20)
           i (iterator-seq (. top20 iterator) )
           ]
+      
       (doall
        (map
         (fn [a b]
-          (println a \space (. b getVertexId) \space (. b getValue)))
+          {:index a
+           :vertex (. b getVertexId)
+           :value (. b getValue)}
+          )
         (range)
         
         i)
        ))
     ))
-
 
 (defn -main
   "I don't do a whole lot."
