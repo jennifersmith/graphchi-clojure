7b069c63339c4dc49b893f9d7e7df44a719249e7
Jennifer Smith <jennifer@home>
2013-01-14 22:25:18 +0000

    Added clojure impl of connected components

--- src/graphchi_clojure/core.clj
+++ src/graphchi_clojure/core.clj
@@ -1,46 +1,55 @@
 (ns graphchi-clojure.core
   (:import [edu.cmu.graphchi GraphChiProgram]
            [edu.cmu.graphchi.engine GraphChiEngine]
-           [edu.cmu.graphchi.datablocks FloatConverter]
+           [edu.cmu.graphchi.datablocks FloatConverter IntConverter]
            [edu.cmu.graphchi.util Toplist
-             IdFloat]))
+             IdFloat LabelAnalysis]))
 
 (set! *warn-on-reflection* true)
 
 (defn all-in-edges-vertices [vertex]
-  (map
-   #(. (. vertex inEdge %) getValue)
-   (range 0 (. vertex numInEdges))))
+  (vec
+   (map
+    #(. (. vertex inEdge %) getValue)
+    (range 0 (. vertex numInEdges)))))
 
 (defn all-out-edges-vertices [vertex]
-  (map
-   #(. (. vertex outEdge %) getValue)
-   (range 0 (. vertex numOutEdges))))
+  (vec
+   (map
+    #(. (. vertex outEdge %) getValue)
+    (range 0 (. vertex numOutEdges)))))
 
 
 
 (defn vertex-to-map [vertex]
+
   {:value (.getValue vertex)
+   :id (.getId vertex)
    :in-edges (all-in-edges-vertices vertex)
    :out-edges (all-out-edges-vertices vertex)})
 
 (defn update-in-edges [vertex in-edges]
-  (map
-   #(. (. vertex inEdge (int %1)) setValue %2)
-   (range)
-   in-edges))
+  (doall
+   (map
+    #(. (. vertex inEdge (int %1)) setValue %2)
+    (range)
+    in-edges)))
 
 (defn update-out-edges [vertex out-edges]
-  (map
-   #(. (. vertex outEdge (int %1)) setValue %2)
-   (range)
-   out-edges))
+  (doall
+   (map
+    #(. (. vertex outEdge (int %1)) setValue %2)
+    (range)
+    out-edges))
+;;  (println "HERE!!!" (all-out-edges-vertices vertex))
+  )
 
-(defn update-vertex [vertex {:keys [value in-edges out-edges]}]
+(defn update-vertex [vertex {:keys [dont-update-in-edges value in-edges out-edges]}]
   (doto vertex
-      (.setValue (float value))
-    (update-in-edges in-edges)
-    (update-out-edges out-edges)))
+      (.setValue (int value)) ;; change to float for the other example!
+    (update-out-edges out-edges))
+  (when-not dont-update-in-edges
+    (update-in-edges vertex in-edges)))
 
 
 (defn calc-new-vertex-value [{:keys [in-edges] :as vertex-map} iteration]
@@ -62,12 +71,62 @@
       (calc-new-vertex-value iteration)
       (calc-out-edges)))
 
+(defn init-cc [{:keys [in-edges out-edges value id]}]
+  {
+   :value id  ;; value as ID
+   :out-edges (repeat (count out-edges) id) ;; set the outbound arrows to be the same as me
+   :dont-update-in-edges  true ;; dodgy
+   :reschedule true ;; process me again in subsequent iter
+   }
+)
+
+(defn run-cc [{:keys [in-edges out-edges value]}]
+  (let [min-in-my-region (apply min value (concat in-edges out-edges))
+        new-in-edges (repeat (count in-edges) min-in-my-region)
+        new-out-edges (repeat (count out-edges) min-in-my-region)
+        ]
+    {
+     :value min-in-my-region ;; minimize
+     :in-edges new-in-edges
+     :out-edges new-out-edges
+     :edge-rescheduling {
+                         :in-edges (map not= new-in-edges in-edges)
+                         :out-edges (map not= new-out-edges out-edges)}}))
+
+
+(defn connected-components [vertex-map iteration]
+  (if (zero? iteration)
+    (init-cc vertex-map)
+    (run-cc vertex-map)))
+
 (defn create-program [update-fn]
   (proxy
       [edu.cmu.graphchi.GraphChiProgram] []
     (update [vertex context]
-      (let [result (update-fn (vertex-to-map vertex) (.getIteration context))]
-        (update-vertex vertex result)))
+      (let [ original (vertex-to-map vertex)
+            result (update-fn original (.getIteration context))
+            scheduler (.getScheduler context)]
+       ;; (println "INPUT " original)
+      ;;  (println "OUTPUT " result)
+        (update-vertex vertex result)
+      ;;  (println "AFTER UPDATE" (vertex-to-map vertex))
+        (when (result :reschedule)
+          (.addTask scheduler (.getId vertex)))
+        (when (result :edge-rescheduling)
+          (let [{:keys [in-edges out-edges]} (result :edge-rescheduling)]
+            (doall
+             (map
+              #(when %1 (.addTask scheduler (.getVertexId (.inEdge vertex %2))))
+              in-edges
+              (range))
+             )
+            (doall
+             (map
+              #(when %1 (.addTask scheduler (.getVertexId (.outEdge vertex %2))))
+              out-edges
+              (range))
+             )
+            ))))
     (beginInterval [context interval] )
     (beginIteration [context])
     (endIteration [context])
@@ -84,6 +143,22 @@
 
     (println "Ready")
     ;; top20 = Toplist.topListFloat(baseFilename, 20);
+
+))
+
+(defn run-with-scheduler [file shards program]
+  (let [engine (new GraphChiEngine file shards)]
+    (doto engine
+      (.setEdataConverter (new IntConverter))
+      (.setVertexDataConverter (new IntConverter))
+      (.setEnableScheduler false)
+      (.run program 5000))
+    (println "Ready")
+    ;; top20 = Toplist.topListFloat(baseFilename, 20);
+
+))
+
+(defn report-top-20 [file]
     (let [top20 (Toplist/topListFloat file 20)
           i (iterator-seq (. top20 iterator) )
           ]
@@ -94,8 +169,16 @@
            :vertex (. b getVertexId)
            :value (. b getValue)}
           )
-        (range) i)))))
+        (range) i))))
 
 (defn -main
   [& args]
-  (run "../facebook/0.edges" 2 (create-program page-rank) 30))
+  (if true
+    (do
+      (run-with-scheduler "../facebook/0.edges" 2 (create-program connected-components))
+      (LabelAnalysis/computeLabels "../facebook/0.edges")
+;;      (map #(vector (.id %) (.count %)))
+      nil)
+    (do
+      (run "../facebook/0.edges" 2 (create-program page-rank) 30)
+      (report-top-20 "../facebook/0.edges"))))
